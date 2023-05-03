2921a79c2886de71192c2fcfd6ec1e0b39f3da37
Changyong IM <imcy0104@gmail.com>
2023-05-04 07:32:59 +0900

    39. 6-4. React에서 배열 사용하기 2 - 데이터 추가하기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import './App.css';
-import DiaryEditor from './DiaryEditor';
+import { useState, useRef } from 'react';
+import './App.css'
+import DiaryEditor from './DiaryEditor'
+import DiaryList from './DiaryList'
+
 
 function App() {
+  const [data, setData] = useState([])
+
+  const dataId = useRef(0)
+
+  const onCreate = (author, content, emotion) => {
+    const created_date = new Date().getTime()
+    const newItem = {
+      author,
+      content,
+      emotion,
+      created_date,
+      id : dataId.current
+    }
+    dataId.current += 1
+    setData([newItem, ...data])
+  }
   return (
     <div className="App">
       <h2>일기장</h2>
-      <DiaryEditor />
+      <DiaryEditor onCreate={onCreate}/>
+      <DiaryList DiaryList={data} />
     </div>
   );
 }
---
+++ src/DiaryList.jsx
@@ -0,0 +1,20 @@
+import DiaryItem from './DiaryItem'
+const DiaryList = ({DiaryList}) => {
+    return (
+        <div className = "DiaryList">
+            <h2>일기 리스트</h2>
+            <h4>{DiaryList.length}개의 일기가 있습니다.</h4>
+            <div>
+                {DiaryList.map((it)=>(
+                    <DiaryItem key={it.id} {...it} />
+                ))}
+            </div>
+        </div>
+    )
+}
+
+DiaryList.defaultProps={
+    DiaryList:[],
+}
+
+export default DiaryList
